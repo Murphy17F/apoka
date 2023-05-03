ccf36b8d47b9a140c527236c80cc287627300583
Murphy17F <murphy17f@gmail.com>
2023-05-03 17:37:38 -0300

    send data

--- redondo/app.js
+++ redondo/app.js
@@ -31,56 +31,27 @@ teams.forEach(team => {
 }); 
 
 function sendData(){
-/*     const formData = new FormData();
-    formData.append("entry.161202834", slot_0);
-    formData.append("entry.572228560", slot_8);
-    formData.append("entry.698483627", slot_1);
-    formData.append("entry.698483627", slot_2);
-    formData.append("entry.698483627", slot_3);
-    formData.append("entry.698483627", slot_4);
-    formData.append("entry.698483627", slot_5);
-    formData.append("entry.698483627", slot_6);
-    formData.append("entry.698483627", slot_7);
-    formData.append("submit", "Submit"); */
     var formId = '1FAIpQLSdKJLGupi4lseiC3JtII8A296fwIG9zMG6JFBY-KGaaL3hdfw'
     var queryString = '/formResponse'
-    var url = 'https://docs.google.com/forms/u/2/d/e/' + formId + queryString
-
-    const formData = new FormData();
-
-    formData.append("entry.161202834", document.getElementById("slot_0").querySelector("img").getAttribute("alt"));
-    for (let i = 1; i <= 7; i++) {
-        formData.append("entry.698483627", document.getElementById("slot_"+i).querySelector("img").getAttribute("alt"));
-    }
-    formData.append("entry.572228560", document.getElementById("slot_8").querySelector("img").getAttribute("alt"));
-
-    const request = new XMLHttpRequest();
-    request.open("POST", url);
-    request.send(formData);
-
-    /* var form = document.createElement("form");
-    var in0 = document.createElement("input"); 
-    var in8 = document.createElement("input"); 
-
+    var url = 'https://docs.google.com/forms/d/e/' + formId + queryString
+    
+    var form = document.createElement("form");
     form.method = "POST";
     form.action = url;   
+    
+    form.appendChild(newInput("entry.161202834", "slot_0"));  
+    form.appendChild(newInput("entry.572228560", "slot_8"));  
 
-    in0.value=document.getElementById("slot_0").querySelector("img").getAttribute("alt");;
-    in0.name="entry.161202834";
-    form.appendChild(in0);  
-
-    in8.value=document.getElementById("slot_8").querySelector("img").getAttribute("alt");;
-    in8.name="entry.572228560";
-    form.appendChild(in8);
-
-    for (let i = 1; i <= 7; i++) {
-        var in0 = document.createElement("input");
-        in0.value = document.getElementById("slot_"+i).querySelector("img").getAttribute("alt");
-        in0.name="entry.698483627";
-        form.appendChild(in0);
-    }
+    for (let i = 1; i <= 7; i++)
+        form.appendChild(newInput("entry.698483627", "slot_"+i));  
 
     document.body.appendChild(form);
+    form.submit();
+}
 
-    form.submit(); */
+function newInput(name, slot){
+    var element = document.createElement("input"); 
+    element.value= document.getElementById(slot).querySelector("img").getAttribute("alt");;
+    element.name = name;
+    return element;
 }
